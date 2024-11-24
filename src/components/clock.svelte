f4314a643bd84c86ad8201fb207cfa7ce77e3c8c
mal1kc <59576495+mal1kc@users.noreply.github.com>
2024-11-24 13:14:25 +0300

    fix: fix #1 analog clock

--- src/components/clock.svelte
+++ src/components/clock.svelte
@@ -3,6 +3,11 @@
   let { timezone = $bindable() }: { timezone: string } = $props();
   const default_tz = "Europe/Istanbul";
 
+  let hour_hand_degree = $state(0);
+  let min_hand_degree = $state(0);
+  let second_hand_degree = $state(0);
+  const second_hand_angular_vel = (1 / 60) * 360;
+
   onMount(() => {
     updateClock(timezone ?? default_tz);
     setInterval(updateClock, 1000);
@@ -13,10 +18,15 @@
   let interval: number;
 
   function updateClock(tz: string): Date {
-    return changeTimeZone(new Date(), tz);
+    let updated_time = changeTimeZone(new Date(), tz);
+    hour_hand_degree =
+      30 * updated_time.getHours() + 0.5 * updated_time.getMinutes();
+    min_hand_degree = updated_time.getMinutes() * 6;
+    second_hand_degree = updated_time.getSeconds() * second_hand_angular_vel;
+    return updated_time;
   }
 
-  function changeTimeZone(date: string | Date, timeZone: string) {
+  function changeTimeZone(date: string | Date, timeZone: string): Date {
     if (typeof date === "string") {
       return new Date(
         new Date(date).toLocaleString("en-US", {
@@ -40,6 +50,8 @@
   onDestroy(() => {
     clearInterval(interval);
   });
+
+  const marks = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
 </script>
 
 <div class="flex flex-col items-center stify-center">
@@ -55,73 +67,45 @@
       stroke-width="10"
       fill="none"
     />
+    {#each marks as mark, index}
+      <line
+        id="clock-min-{mark}"
+        x1="10"
+        y1="100"
+        x2="25"
+        y2="100"
+        stroke="lightgray"
+        stroke-width="2"
+        transform="rotate({index * 30} 100 100)"
+      />
+    {/each}
 
     <line
-      id="clock-min-45"
-      x1="100"
-      y1="175"
-      x2="100"
-      y2="190"
-      stroke="lightgray"
-      stroke-width="2"
-    />
-    <line
-      id="clock-min-15"
-      x1="100"
-      y1="25"
-      x2="100"
-      y2="10"
-      stroke="lightgray"
-      stroke-width="2"
-    />
-
-    <line
-      id="clock-min-30"
-      x1="190"
+      x1="45"
       y1="100"
-      x2="175"
+      x2="100"
       y2="100"
-      stroke="lightgray"
-      stroke-width="2"
-    />
-
-    <line
-      id="clock-min-0"
-      x1="10"
-      y1="100"
-      x2="25"
-      y2="100"
-      stroke="lightgray"
-      stroke-width="2"
-    />
-
-    <line
-      x1="100"
-      y1="100"
-      x2="100"
-      y2="10"
       stroke="black"
       stroke-width="2"
-      transform="rotate({(time.getHours() % 12) * 30 +
-        time.getMinutes() * 0.5} 100 100)"
+      transform="rotate({hour_hand_degree} 100 100)"
     />
     <line
-      x1="100"
+      x1="20"
       y1="100"
       x2="100"
-      y2="30"
+      y2="100"
       stroke="black"
       stroke-width="2"
-      transform="rotate({time.getMinutes() * 6} 100 100)"
+      transform="rotate({min_hand_degree} 100 100)"
     />
     <line
-      x1="100"
+      x1="15"
       y1="100"
       x2="100"
-      y2="50"
+      y2="100"
       stroke="red"
       stroke-width="2"
-      transform="rotate({time.getSeconds() * 6} 100 100)"
+      transform="rotate({second_hand_degree} 100 100)"
     />
   </svg>
   <div class="flex mt-10 items-center justify-center font-bold">
