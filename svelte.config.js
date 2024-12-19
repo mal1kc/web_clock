c2e652f86efec8ccbe9f5da20695f7203850ebf6
mal1kc <59576495+mal1kc@users.noreply.github.com>
2024-12-19 22:55:43 +0300

    fix: dumb mistake

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ export default {
       // these options are set automatically — see below
       pages: 'build',
       assets: 'build',
-      fallback: '404.html',
+      fallback: 'index.html',
       precompress: true,
       strict: true
     }),
